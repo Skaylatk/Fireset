35ce9de69eeaeff30d4986af5b4d9b3aef76cb44
saintsHr <hiqkrs@gmail.com>
2025-12-19 16:02:37 -0300

    doc: fixed install instructions

--- include/fireset/fireset.h
+++ include/fireset/fireset.h
@@ -33,11 +33,11 @@
  * - go to Releases tab and install the latest stable release
  * - install your .deb:
  * ```bash
- * sudo apt install ./fireset-dev_<version>_amd64.deb
+ * sudo apt install ./fireset-dev.deb
  * ```
  * - now use those flags in your compilation:
  * ```bash
- * -Ifireset -lfireset -lglfw -lGL
+ * -Ifireset -lfireset -lglfw -lGL -lm
  * ```
  * or:
  * ```bash
@@ -57,7 +57,7 @@
  * - move those to any place you like
  * - now use those flags in your compilation:
  * ```bash
- * -Lpath/to/lib/folder -lfireset -Ifireset -lfireset -lglfw -lGL -lm -Ipath/to/include/folder
+ * -Lpath/to/lib/folder -lfireset -lglfw -lGL -lm -Ipath/to/include/folder
  * ```
  * - if you get any linking errors related to OpenGL or GLFW, make sure you
  * have it installed and also add a "-L" flag for those
