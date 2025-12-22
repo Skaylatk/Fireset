9523e9229c6d036fe3a62de349f2fa87ec1a02da
saintsHr <hiqkrs@gmail.com>
2025-12-22 16:24:08 -0300

    feat(physics): initial features

--- include/fireset/physics.h
+++ include/fireset/physics.h
@@ -3,10 +3,63 @@
 #include <chipmunk/chipmunk.h>
 
 #include "fireset/vertex.h"
+#include "fireset/window.h"
+
+typedef enum{
+    FS_BODY_STATIC,
+    FS_BODY_KINEMATIC,
+    FS_BODY_DYNAMIC
+}FsBodyType;
+
+typedef enum{
+    FS_SHAPE_CIRCLE,
+    FS_SHAPE_BOX,
+    FS_SHAPE_LINE
+}FsShapeType;
 
 typedef struct{
     cpSpace* raw;
     FsVec2 gravity;
 }FsSpace;
 
+typedef struct{
+    cpBody* raw;
+    FsBodyType type;
+}FsBody;
+
+typedef struct{
+    FsShapeType type;
+    union{
+        struct{
+            float radius;
+            cpVect offset;
+        }circle;
+
+        struct{
+            cpVect a, b;
+            float radius;
+        }segment;
+
+        struct{
+            float width, height;
+            float radius;
+        }box;
+    };
+}FsShapeDesc;
+
+typedef struct{
+    cpShape* raw;
+    FsShapeType type;
+}FsShape;
+
 FsSpace fsCreateSpace(FsVec2 gravity);
+FsBody fsCreateBody(FsBodyType type, FsSpace* space);
+FsShape fsCreateShape(FsBody* body, const FsShapeDesc* desc);
+void fsSpaceAddShape(FsSpace* space, FsShape* shape);
+void fsSpaceAddBody(FsSpace* space, FsBody* body);
+void fsShapeSetFriction(FsShape* shape, float friction);
+void fsBodySetMoment(FsBody* body, float moment);
+void fsBodySetMass(FsBody* body, float mass);
+void fsBodySetPosition(FsBody* body, FsVec2 position);
+void fsSpaceStep(FsSpace* space);
+FsVec2 fsBodyGetPosition(FsBody* body);
--- src/physics.c
+++ src/physics.c
@@ -6,3 +6,95 @@ FsSpace fsCreateSpace(FsVec2 gravity){
     cpSpaceSetGravity(space.raw, cpv(gravity.x, gravity.y));
     return space;
 }
+
+FsBody fsCreateBody(FsBodyType type, FsSpace* space){
+    FsBody body = {0};
+    body.type = type;
+
+    switch (type){
+        case FS_BODY_STATIC:
+            body.raw = cpSpaceGetStaticBody(space->raw);
+            break;
+        case FS_BODY_KINEMATIC:
+            body.raw = cpBodyNewKinematic();
+            break;
+        case FS_BODY_DYNAMIC:
+            body.raw = cpBodyNew(1.0f, 1.0f);
+            break;
+        default:
+            break;
+    }
+
+    return body;
+}
+
+FsShape fsCreateShape(FsBody* body, const FsShapeDesc* desc){
+    FsShape shape = {0};
+
+    switch (desc->type){
+    case FS_SHAPE_BOX:
+        shape.type = FS_SHAPE_BOX;
+        shape.raw = cpBoxShapeNew(
+            body->raw,
+            desc->box.width,
+            desc->box.height,
+            desc->box.radius
+        );
+        break;
+    case FS_SHAPE_CIRCLE:
+        shape.type = FS_SHAPE_CIRCLE;
+        shape.raw = cpCircleShapeNew(
+            body->raw,
+            desc->circle.radius,
+            desc->circle.offset
+        );
+        break;
+    case FS_SHAPE_LINE:
+        shape.type = FS_SHAPE_LINE;
+        shape.raw = cpSegmentShapeNew(
+            body->raw,
+            desc->segment.a,
+            desc->segment.b,
+            desc->segment.radius
+        );
+        break;
+    default:
+        break;
+    }
+
+    return shape;
+}
+
+void fsSpaceAddShape(FsSpace* space, FsShape* shape){
+    cpSpaceAddShape(space->raw, shape->raw);
+}
+
+void fsSpaceAddBody(FsSpace* space, FsBody* body){
+    cpSpaceAddBody(space->raw, body->raw);
+}
+
+void fsShapeSetFriction(FsShape* shape, float friction){
+    cpShapeSetFriction(shape->raw, friction);
+}
+
+void fsBodySetMass(FsBody* body, float mass){
+    cpBodySetMass(body->raw, mass);
+}
+
+void fsBodySetMoment(FsBody* body, float moment){
+    cpBodySetMoment(body->raw, moment);
+}
+
+void fsBodySetPosition(FsBody* body, FsVec2 position){
+    cpBodySetPosition(body->raw, cpv(position.x, position.y));
+}
+
+void fsSpaceStep(FsSpace* space){
+    cpSpaceStep(space->raw, 1.0 / 60.0);
+}
+
+FsVec2 fsBodyGetPosition(FsBody* body){
+    cpVect cpvec = cpBodyGetPosition(body->raw);
+    FsVec2 vec = FsVec2_new(cpvec.x, cpvec.y);
+    return vec;
+}
